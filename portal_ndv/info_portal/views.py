5a25d70a8cb3e5a321ee36231b64cb01ed3b66ef
VIVOG <vivog2017@gmail.com>
2022-06-17 21:17:23 +0300

    edit first view

--- portal_ndv/info_portal/views.py
+++ portal_ndv/info_portal/views.py
@@ -14,16 +14,14 @@ from .models import *
 # Create your views here.
 
 def home(request):
-    num_divisions = Divisions.objects.all().count()
-    num_staff = Staff.objects.all().count()
-    num_doc = Documents.objects.all().count()
-    context = {
-        'num_divisions': num_divisions,
-        'num_staff': num_staff,
-        'num_doc': num_doc}
-    return render(request, 'nio_app/home.html', context=context)
+    # num_divisions = Divisions.objects.all().count()
+    # num_staff = Staff.objects.all().count()
+    # num_doc = Documents.objects.all().count()
+    # context = {
+    #     'num_divisions': num_divisions,
+    #     'num_staff': num_staff,
+    #     'num_doc': num_doc}
+    # return render(request, 'nio_app/home.html', context=context)
+    return render(request, 'info_portal/index.html')
 
 
-from django.shortcuts import render
-
-# Create your views here.
